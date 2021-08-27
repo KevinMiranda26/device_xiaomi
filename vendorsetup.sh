beadf7b9f78a6c74a27ca01a54e93ff225e26df3
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-08-27 18:47:45 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #vendor
 git clone https://github.com/FabricioSC123/android_vendor_xiaomi_surya.git vendor/xiaomi/surya
 #kernel
-git clone https://github.com/FabricioSC123/kernel_xiaomi_surya-1.git kernel/xiaomi/surya
+git clone https://github.com/FabricioSC123/kernel_xiaomi_surya.git kernel/xiaomi/surya
 #firware
 git clone https://gitlab.com/FabricioSC123/firmware_xiaomi_surya.git firmware/xiaomi/surya
 #PocoParts
