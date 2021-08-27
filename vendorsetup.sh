9afc4730ddf2f942cc8030bd306b52a9f1317782
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-08-27 01:30:52 -0500

    Add repos needed

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,23 @@
 #vendor
 git clone https://github.com/FabricioSC123/android_vendor_xiaomi_surya.git vendor/xiaomi/surya
 #kernel
-git clone https://github.com/FabricioSC123/kernel_xiaomi_surya.git kernel/xiaomi/surya
+git clone https://github.com/FabricioSC123/kernel_xiaomi_surya-1.git kernel/xiaomi/surya
 #firware
 git clone https://gitlab.com/FabricioSC123/firmware_xiaomi_surya.git firmware/xiaomi/surya
 #PocoParts
 git clone https://github.com/FabricioSC123/vendor_PocoParts.git vendor/PocoParts
 #VendorSupport
 git clone https://github.com/FabricioSC123/android_vendor_support.git vendor/support
+
+# Proton
+git clone https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-13 --depth=1
+touch prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
+echo "13" >> prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
+
+# Disable Realme partitions
+git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
+git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
+
+# Display Hals
+git -C hardware/qcom-caf/sm8150/display remote add pixel https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display
+git -C hardware/qcom-caf/sm8150/display fetch pixel && git -C hardware/qcom-caf/sm8150/display checkout pixel/eleven
