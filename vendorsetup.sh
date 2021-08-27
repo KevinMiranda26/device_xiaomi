fd9c423ab0eb014733d8e400a56577c20b04378c
FabricioSC123 <89610142+FabricioSC123@users.noreply.github.com>
2021-08-27 02:53:29 -0500

    Add hardware/xiaomi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,8 @@ echo "13" >> prebuilts/clang/host/linux-x86/clang-13/AndroidVersion.txt
 git -C build/tools fetch https://github.com/Evolution-X/build elle 75895232a2b045431589bcb31ed808cc86ebffa4
 git -C build/tools revert 75895232a2b045431589bcb31ed808cc86ebffa4 --no-edit
 
+# Hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
 # Display Hals
 git -C hardware/qcom-caf/sm8150/display remote add pixel https://github.com/PixelExperience/hardware_qcom-caf_sm8150_display
 git -C hardware/qcom-caf/sm8150/display fetch pixel && git -C hardware/qcom-caf/sm8150/display checkout pixel/eleven
